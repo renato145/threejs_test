462dcf43afdb6abdc66d35aa3b4bfc6da862e202
Renato Hermoza <renato.hermoza@pucp.edu.pe>
2020-01-19 00:26:31 +1030

    first animation

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import 'bootstrap/dist/css/bootstrap.css';
 import React, { useRef, useMemo, useState, useEffect } from 'react';
 import ReactDOM from 'react-dom';
-import { Canvas, useThree, useUpdate } from 'react-three-fiber'
+import { Canvas, useThree, useUpdate, useFrame } from 'react-three-fiber';
+import { animated, useSpring } from 'react-spring-three';
 import './index.css';
 const THREE = require('three');
 // const d3 = require('d3');
@@ -11,7 +12,7 @@ const width = 800;
 const fov = 30;
 const near = 10;
 const far = 100;
-const nPoints = 100;
+const nPoints = 200;
 const randomScale = 40;
 const pointsSize = 5;
 
@@ -24,16 +25,34 @@ const getRandomPoints = () => {
     positions.push(getRandomNumber());
     positions.push(0);
   });
-  return new Float32Array(positions, 3);
+  return positions;
 };
 
 const Scene = ({ points }) => {
   const ref = useRef();
   const { scene, camera } = useThree();
+  const [ hovered, setHover] = useState(false);
 
   const geometryRef = useUpdate( geometry => {
     geometry.attributes.position.needsUpdate = true;
   }, [ points ]);
+
+  const hoverProps = useSpring({
+    'material-size': hovered ? pointsSize*2 : pointsSize,
+  });
+
+  const [ { positions }, setSpring ] = useSpring(() => ({
+    positions: new Array(nPoints*3).fill(0)
+  }));
+
+  useEffect(() => {
+    setSpring({ positions: points });
+  }, [ points, setSpring ]);
+
+  useFrame(() => {
+    geometryRef.current.attributes.position.array = new Float32Array(positions.getValue(), 3);
+    geometryRef.current.attributes.position.needsUpdate = true;
+  });
 
   camera.fov = fov;
   camera.aspect = width / height;
@@ -47,18 +66,23 @@ const Scene = ({ points }) => {
     <mesh
       ref={ref}
     >
-      <points>
+      <animated.points
+        onClick={() => setHover(!hovered)}
+        onHover={() => setHover(true)}
+        onUnHover={() => setHover(false)}
+        {...hoverProps}
+      >
         <bufferGeometry attach='geometry' ref={geometryRef}>
           <bufferAttribute
             attachObject={['attributes', 'position']}
             count={points.length / 3}
-            array={points}
+            array={new Float32Array(positions.getValue(), 3)}
             itemSize={3}
             usage={THREE.DynamicDrawUsage}
           />
           </bufferGeometry>
         <pointsMaterial attach='material' size={pointsSize} color='hotpink' />
-      </points>
+      </animated.points>
     </mesh>
   );
 };
