7a769c4b4a6ec35ce28ba25fa035900377874264
Renato Hermoza <renato.hermoza@pucp.edu.pe>
2020-01-18 23:09:27 +1030

    refresh button

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
-import React, { useRef, useMemo } from 'react';
+import 'bootstrap/dist/css/bootstrap.css';
+import React, { useRef, useMemo, useState, useEffect } from 'react';
 import ReactDOM from 'react-dom';
-import { Canvas, useThree } from 'react-three-fiber'
+import { Canvas, useThree, useUpdate } from 'react-three-fiber'
 import './index.css';
 const THREE = require('three');
 // const d3 = require('d3');
@@ -10,51 +11,29 @@ const width = 800;
 const fov = 30;
 const near = 10;
 const far = 100;
-const maxPoints = 5000;
 const nPoints = 100;
 const randomScale = 40;
 const pointsSize = 5;
 
 const getRandomNumber = () => (Math.random()-0.5)*randomScale;
 
-const Scene = () => {
+const getRandomPoints = () => {
+  const positions = []
+  Array(nPoints).fill().forEach(() => {
+    positions.push(getRandomNumber());
+    positions.push(getRandomNumber());
+    positions.push(0);
+  });
+  return new Float32Array(positions, 3);
+};
+
+const Scene = ({ points }) => {
   const ref = useRef();
   const { scene, camera } = useThree();
 
-  const points = useMemo(() => { 
-    const positions = []
-    Array(nPoints).fill().forEach(() => {
-      positions.push(getRandomNumber());
-      positions.push(getRandomNumber());
-      positions.push(0);
-    });
-
-    return new Float32Array(positions, 3)
-  }, []);
-
-  // {
-  //   const positions = d3.range(nPoints).map(() => ({
-  //     x: (Math.random()-0.5)*40,
-  //     y: (Math.random()-0.5)*40,
-  //   }));
-  //   const pointsGeometry = new THREE.Geometry();
-  //   const colors = [];
-  //   positions.forEach(({ x, y }) => {
-  //     const vertex = new THREE.Vector3(x, y, 0);
-  //     pointsGeometry.vertices.push(vertex);
-  //     const color = new THREE.Color('hotpink');
-  //     colors.push(color);
-  //   });
-  //   pointsGeometry.colors = colors;
-  //   const pointsMaterial = new THREE.PointsMaterial({
-  //     size: 8,
-  //     sizeAttenuation: false,
-  //     vertexColors: THREE.VertexColors,
-  //   });
-  //   // TODO: Sprite settings
-
-  //   return new THREE.Points(pointsGeometry, pointsMaterial);
-  // }, []);
+  const geometryRef = useUpdate( geometry => {
+    geometry.attributes.position.needsUpdate = true;
+  }, [ points ]);
 
   camera.fov = fov;
   camera.aspect = width / height;
@@ -69,12 +48,13 @@ const Scene = () => {
       ref={ref}
     >
       <points>
-        <bufferGeometry attach='geometry' >
+        <bufferGeometry attach='geometry' ref={geometryRef}>
           <bufferAttribute
             attachObject={['attributes', 'position']}
             count={points.length / 3}
             array={points}
             itemSize={3}
+            usage={THREE.DynamicDrawUsage}
           />
           </bufferGeometry>
         <pointsMaterial attach='material' size={pointsSize} color='hotpink' />
@@ -84,12 +64,22 @@ const Scene = () => {
 };
 
 const App = () => {
+  const [ points, setPoints ] = useState(getRandomPoints());
+
   return (
     <div style={{width: width, height: height}}>
-      <p>Testing :)</p>
       <Canvas>
-        <Scene />
+        <Scene points={points}/>
       </Canvas>
+      <div className='button-container'>
+        <button
+          type='button'
+          className='btn btn-primary'
+          onClick={() => setPoints(getRandomPoints())}
+        >
+          Refresh
+        </button>
+      </div>
     </div>
   )
 };
