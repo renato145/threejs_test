4a1b9fd344d21ba7f4bf8c002af0a331b0e18ae1
Renato Hermoza <renato.hermoza@pucp.edu.pe>
2020-01-25 19:56:39 +1030

    fix spaces

--- src/useCustomHover.js
+++ src/useCustomHover.js
@@ -11,32 +11,32 @@ export const useCustomHover = ({
   onPointHover,
   onPointOut
   }) => {
-    useEffect(() => {
-      const view = select(renderer.domElement);
-      view.on('mousemove', () => {
-        checkIntersects(mouse);
-      });
-    }, [ renderer, mouse ]);
-    
-    const raycaster = useMemo(() => {
-      const raycaster = new THREE.Raycaster();
-      raycaster.params.Points.threshold = 1;
-      return raycaster;
-    }, []);
-    
-    const checkIntersects = position => {
-      raycaster.setFromCamera(position, camera);
-      let intersects = raycaster.intersectObject(pointsRef.current);
-      if (intersects[0]) {
-        const intersect = intersects.sort(({ distanceToRay }) => distanceToRay)[0];
-        const point = intersect.point;
-        point.project(camera).addScalar(1).divideScalar(2);
-        const x = point.x * width;
-        const y = (1-point.y) * height;
-        onPointHover({ index: intersect.index, x, y });
-      } else {
-        onPointOut();
-      }
-    };
+  useEffect(() => {
+    const view = select(renderer.domElement);
+    view.on('mousemove', () => {
+      checkIntersects(mouse);
+    });
+  }, [ renderer, mouse ]);
+
+  const raycaster = useMemo(() => {
+    const raycaster = new THREE.Raycaster();
+    raycaster.params.Points.threshold = 1;
+    return raycaster;
+  }, []);
+
+  const checkIntersects = position => {
+    raycaster.setFromCamera(position, camera);
+    let intersects = raycaster.intersectObject(pointsRef.current);
+    if (intersects[0]) {
+      const intersect = intersects.sort(({ distanceToRay }) => distanceToRay)[0];
+      const point = intersect.point;
+      point.project(camera).addScalar(1).divideScalar(2);
+      const x = point.x * width;
+      const y = (1-point.y) * height;
+      onPointHover({ index: intersect.index, x, y });
+    } else {
+      onPointOut();
+    }
+  };
 };
 
